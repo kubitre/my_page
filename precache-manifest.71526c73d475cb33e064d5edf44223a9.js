66c4dbb0ab465c7c37a4232a608c9ef27f0ec5e5
kubitre <kubitre@gmail.com>
2019-01-05 05:04:28 +0700

    Updates

---
+++ precache-manifest.71526c73d475cb33e064d5edf44223a9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my_page/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "dc94915c93f7fbde5cd6a600723b463e",
+    "url": "/my_page/static/media/NoPath@2x.dc94915c.png"
+  },
+  {
+    "revision": "7522894ec3c199f3e195ad4d005c24e3",
+    "url": "/my_page/static/media/Group 1@2x.7522894e.png"
+  },
+  {
+    "revision": "4a2b1b5f4b6aaee20131",
+    "url": "/my_page/static/js/runtime~main.4a2b1b5f.js"
+  },
+  {
+    "revision": "e8fad1fff82ff98489ac",
+    "url": "/my_page/static/js/main.e8fad1ff.chunk.js"
+  },
+  {
+    "revision": "64162b2fc7875ae27cb0",
+    "url": "/my_page/static/js/1.64162b2f.chunk.js"
+  },
+  {
+    "revision": "e8fad1fff82ff98489ac",
+    "url": "/my_page/static/css/main.feb13e20.chunk.css"
+  },
+  {
+    "revision": "8e0dc7d74a993f62e22a06b4d07d1632",
+    "url": "/my_page/index.html"
+  }
+];
